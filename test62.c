e1924ab3872703d929be25f6d7c2e8f11200161f
Darling86 <1570906137@qq.com>
2022-11-20 09:15:16 +0800

    6

--- test62.c
+++ test62.c
@@ -7,4 +7,11 @@ int main (void)
 
     printf("cnum:%d,&cnum:%p\n",cnum,&cnum);
     printf("*pc:%d\n,pc:%p",*pc,pc);
+
+    num = 1024;   //if *pc=1024 这是禁止的，无法修改const修饰的变量
+    printf("*pc:%d\n",*pc);
+    printf("num:%d\n",num);
+    
+    return 0;
+
 }
