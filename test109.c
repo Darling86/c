29f2328aaa2cd8e2210e84c4d033311163c815f6
darling86 <1570906137@qq.com>
2023-02-20 13:12:43 +0800

    C6

--- test109.c
+++ test109.c
@@ -1,1 +1,20 @@
+//6.5.1传递结构体变量(直接赋值)
 #include<stdio.h>
+
+int main(void)
+{
+    struct Test
+    {
+        int x;
+        int y;
+    }t1,t2;
+
+    t1.x=3;
+    t1.y=4;
+
+    t2=t1;
+
+    printf("t1.x=%d,t2.y=%d\n",t2.x,t2.y);
+
+    return 0;
+}
